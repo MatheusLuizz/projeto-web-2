06e35c8e453ee0352dcd6069c56092a22aaf81b5
Adriel <adriel_leite12@outlook.com>
2024-08-14 22:05:09 -0300

    Tabelas do banco de dados

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,3 +11,41 @@ class Project(models.Model):
     
     def __str__(self):
         return self.name
+    
+from django.db import models
+
+class Usuario(models.Model):
+    cpf = models.CharField(max_length=255, primary_key=True)
+    nome = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=255)
+    faturamento = models.FloatField()
+    criacao_conta = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Ganho(models.Model):
+    cliente_cpf = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nome_atividade = models.CharField(max_length=255)
+    tipo_atividade = models.CharField(max_length=255)
+    valor = models.FloatField()
+    data = models.DateField()
+    descricao = models.CharField(max_length=500)
+    recorrencia = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome_atividade
+
+
+class Gasto(models.Model):
+    cliente_cpf = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nome_atividade = models.CharField(max_length=255)
+    tipo_atividade = models.CharField(max_length=255)
+    valor = models.FloatField()
+    data = models.DateField()
+    descricao = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.nome_atividade
