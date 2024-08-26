bd5cd2876b5411486451bb74c8a6bb99258c99bd
asfmur <masf5@discente.ifpe.edu.br>
2024-08-26 17:22:23 -0300

    Revert "\adicionei arquivos da dashboard \adicionei e serializers views dos ganhos, users e gastos \ adicionei urls /users /ganhos /gastos"
    This reverts commit 80137403c3e3831b72cb88b912a3a9e57c73e311.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,19 +5,3 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ('name', 'start_date', 'end_date', 'comments', 'status')
-
-
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ('cpf', 'nome', 'email', 'telefone', 'faturamento', 'criacao_conta')
-
-class GanhoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ganho
-        fields = ('cliente_cpf', 'nome_atividade', 'tipo_atividade', 'valor', 'data', 'descricao', 'recorrencia')
-
-class GastoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Gasto
-        fields = ('cliente_cpf', 'nome_atividade', 'tipo_atividade', 'valor', 'data', 'descricao')
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,25 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from .views import *
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('projects', ProjectViewSet, basename='project')
-
-router2 = DefaultRouter()
-router2.register('users', UserViewSet, basename='users')
-
-router3 = DefaultRouter()
-router3.register('ganhos', GanhoViewSet, basename='ganhos')
-
-router4 = DefaultRouter()
-router4.register('gastos', GastoViewSet, basename='gastos')
-urlpatterns = [ 
-    
-    path('', include(router.urls)),
-    path('', include(router2.urls)),
-    path('', include(router3.urls)),
-    path('', include(router4.urls))
-    ]
+urlpatterns = router.urls
 
 #urlpatterns = [
 #    
