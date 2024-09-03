b1d9b2c5fec65efec5e9b29d0cb819892fa3d69b
Matheus Luiz <matheus.luizf35@gmail.com>
2024-09-03 20:59:09 -0300

    Alterações de configurações para deploy na Nuvem

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud.settings')
+    settings_module = "projectapi.production" if 'WEBSITE_HOSTNAME' in os.environ else 'crud.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
